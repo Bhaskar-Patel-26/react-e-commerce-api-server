c738c9978ae47a6cfeb45645148e3dfab1c5b304
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2025-02-02 22:03:08 +0530

    authAPI usign RTK query is completed

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import express from "express";
 import connectDB from "./utils/connectDB.js";
 import { configDotenv } from "dotenv";
 import userRouters from "./routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 configDotenv();
 const port = 3000;
-connectDB()
+connectDB();
 
 // Middlewares
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Test Page");
